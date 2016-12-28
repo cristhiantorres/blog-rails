526708c1e57066198af22f6dbbbefae01e9b2af3
Cristhian Torres <cristhiantorres95@gmail.com>
2016-12-28 15:36:55 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :articles, except: [:delete]
+=begin
+  	get "/articles"
+  	post "/articles"
+  	delete "/articles"
+  	get "/articles/:id"
+  	get "/articles/new"
+  	get "/articles/:id/edit"
+  	patch "/articles/:id"
+  	put "/articles/:id"
+=end
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
